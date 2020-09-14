fc50d5f2382552a2211eda29b4fea4020f1b5fab
yarrou <konic2884@gmail.com>
2020-09-14 18:27:06 +0300

    Initial commit

---
+++ src/test/java/org/change/springLesson/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.change.springLesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
