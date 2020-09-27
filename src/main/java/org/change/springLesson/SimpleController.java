36e23a0394b1bd5169093abce670ad92423ead5d
yarrou <konic2884@gmail.com>
2020-09-27 20:54:00 +0300

    inicial

--- src/main/java/org/change/springLesson/SimpleController.java
+++ src/main/java/org/change/springLesson/SimpleController.java
@@ -1,18 +1,44 @@
 package org.change.springLesson;
 
+import org.change.springLesson.models.Product;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.ArrayList;
+
+@RestController
 public class SimpleController {
-    @Value("${spring.application.name}")
-    String appName;
+    private ArrayList<Product> list = DataBase.getInstance().getList();
 
-    @GetMapping("/")
-    public String homePage(Model model) {
-        model.addAttribute("appName", appName);
-        return "home";
+    @GetMapping("/shop/product/id")
+    public String getProduct(@RequestParam(required = false) String id) {
+        return "Product : " + list.get(Integer.parseInt(id)).toString();
+    }
+
+    @GetMapping("/shop/product/name")
+    public String getIdProduct(@RequestParam (required = false) String name) {
+        int idProd = -1;
+        boolean verefi=false;
+        for(Product product :list){
+            idProd++;
+            if(name.equals(product.getName())){
+                verefi=true;
+                break;
+            }
+        }
+        if(verefi){
+            return "Product id: " + idProd;
+        }
+        else {
+            return "Not found";
+        }
+    }
+
+    @PostMapping("/shop/product/newproduct")
+    public String addProduct(@RequestParam String productname,@RequestParam int cost) {
+        list.add(new Product(productname,cost));
+        return "product: " + productname + " added";
     }
 }
