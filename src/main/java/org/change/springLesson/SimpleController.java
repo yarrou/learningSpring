beace391cfcf4473d1434e7e2a73b496a7f3287f
yarrou <konic2884@gmail.com>
2020-09-28 22:02:41 +0300

    solved tasks 3

--- src/main/java/org/change/springLesson/SimpleController.java
+++ src/main/java/org/change/springLesson/SimpleController.java
@@ -1,6 +1,8 @@
 package org.change.springLesson;
 
 import org.change.springLesson.models.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,14 +12,18 @@ public class SimpleController {
     private ArrayList<Product> list = DataBase.getInstance().getList();
 
     @GetMapping("/shop/product/{id}")
-    public String getProduct(@PathVariable String id) {
-        return "{\"id\": " + id + ',' + list.get(Integer.parseInt(id)).toString() + '}';
+    public ResponseEntity getProduct(@PathVariable String id) {
+        if (Integer.parseInt(id) < 0 || Integer.parseInt(id) > (list.size() - 1)) {
+            return new ResponseEntity<>("{\"status\": \"Not Found\"}", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity("{\"id\": " + id + ',' + list.get(Integer.parseInt(id)).toString() + '}', HttpStatus.OK);
+        }
     }
 
 
     @PostMapping("/shop/product/newproduct")
-    public String addProduct(@RequestParam String productname, @RequestParam int cost) {
+    public ResponseEntity addProduct(@RequestParam String productname, @RequestParam int cost) {
         list.add(new Product(productname, cost));
-        return "product: " + productname + " added";
+        return new ResponseEntity("product: " + productname + " added", HttpStatus.OK);
     }
 }
